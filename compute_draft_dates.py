ee8e89114e6aca66a7625c2d774129042cd32b4e
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2019-12-03 16:03:54 -0800

    add dates stuff

---
+++ compute_draft_dates.py
@@ -0,0 +1,80 @@
+﻿import math, json, datetime
+f = open("drafts.tsv", "r", encoding="utf-8")
+      
+playeridx = 0
+draftidx = 0
+
+date_format = "%Y-%m-%d"
+carddict = {}
+card_release_dates = {}
+with open("../scryfall-default-cards.json", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+    for card in data:
+        legal = card['legalities']['vintage']
+        if legal == 'legal' or legal == 'restricted':
+            name = card['name'].replace(u"û", "u").replace(u"ö", "o").replace(u"á", "a").replace(u"é", "e").replace(u"à", "a").replace(u"â", "a").replace(u"ú", "u").replace(u"í", "i")
+            if card['layout'] == 'transform' or card['layout'] == 'flip' or card['layout'] == 'adventure':
+                idx = name.find(" // ")
+                if idx > -1:
+                    name = name[:idx]
+            name = name.lower()
+            
+            #find the earliest date each card was released
+            carddatestr = card['released_at']            
+            carddate = datetime.datetime.strptime(carddatestr, date_format)
+            if name not in card_release_dates.keys() or card_release_dates[name] > carddate:
+                card_release_dates[name] = carddate
+            carddict[name] = card        
+
+current_draft_id = None
+current_draft_date = None
+current_draft_cards = set()
+current_line = 0
+
+outf = open("draft dates output.tsv", "w")
+for line in f:
+    current_line += 1
+    elems = line.rstrip().split("\t")
+    draftinfo = elems[0] #  the draft id or date, on the first and second rows
+    if playeridx == 0:
+        if not draftinfo:
+            raise Exception("Missing draft id on line " + str(current_line))
+                        
+        current_draft_id = draftinfo
+        outf.write(current_draft_id + "\t")
+        
+    elif playeridx == 1:
+        current_draft_date = draftinfo
+    # elems[1] is player name which I don't care about right now
+    wins = int(elems[2])
+    losses = int(elems[3])
+
+    cardcount = 0
+    for card in elems[4:]:
+        card = card.strip()
+        card = card.lower()
+        if not card:
+            continue
+                    
+        current_draft_cards.add(card)       
+
+        cardcount += 1
+           
+    playeridx += 1
+    if playeridx >= 8:
+        playeridx = 0
+        latest_date = datetime.datetime(1990, 1, 1)
+        for card in current_draft_cards:
+            if card_release_dates[card] > latest_date:
+                latest_date = card_release_dates[card]
+        outf.write(latest_date.strftime(date_format) + "\n")
+        current_draft_cards.clear()
+            
+
+
+
+outf.close()
+
+    
+        
+    
--- compute_vrd_stats.py
+++ compute_vrd_stats.py
@@ -1,4 +1,4 @@
-﻿import math, json
+﻿import math, json, datetime
 f = open("drafts.tsv", "r", encoding="utf-8")
       
 playeridx = 0
@@ -11,9 +11,11 @@ winpows = {}
 losspows = {}
 seatwins = [0] * 8
 seatlosses =[0] * 8
+card_release_dates = {}
+date_format = "%Y-%m-%d"
 
 carddict = {}
-with open("../scryfall-oracle-cards.json", encoding="utf-8") as json_file:
+with open("../scryfall-default-cards.json", encoding="utf-8") as json_file:
     data = json.load(json_file)
     for card in data:
         legal = card['legalities']['vintage']
@@ -24,11 +26,37 @@ with open("../scryfall-oracle-cards.json", encoding="utf-8") as json_file:
                 if idx > -1:
                     name = name[:idx]
             name = name.lower()
-            carddict[name] = card        
+            carddict[name] = card
+            
+            #find the earliest date each card was released
+            carddatestr = card['released_at']
+            carddate = datetime.datetime.strptime(carddatestr, date_format)
+            if name not in card_release_dates.keys() or card_release_dates[name] > carddate:
+                card_release_dates[name] = carddate
+            carddict[name] = card
 
+current_draft_id = None
+current_draft_date = None
+current_line = 0
+draft_dates = []
 for line in f:    
+    current_line += 1
     elems = line.rstrip().split("\t")
-    draftinfo = elems[1] #  the draft id or date, on the first and second rows
+    draftinfo = elems[0] #  the draft id or date, on the first and second rows
+    if playeridx == 0:
+        if not draftinfo:
+            raise Exception("Missing draft id and date on line " + str(current_line))
+        infos = draftinfo.split(", ")
+        if len(infos) != 2:
+            raise Exception("Line " + str(current_line) + ": doesn't have draftid,date at the front")
+        current_draft_id = infos[0]
+        current_draft_date_str = infos[1]
+        if not current_draft_id or not current_draft_date_str:
+            raise Exception("Line " + str(current_line) + ": doesn't have draftid,date at the front")
+        current_draft_date = datetime.datetime.strptime(current_draft_date_str, date_format)
+        draft_dates.append(current_draft_date)        
+        
+    # elems[1] is player name        
     wins = int(elems[2])
     losses = int(elems[3])
     seatwins[playeridx] += wins
@@ -41,6 +69,9 @@ for line in f:
         card = card.lower()
         if not card:
             continue
+                
+        if card_release_dates[card] > current_draft_date:
+            raise Exception(card + " was released on " + card_release_dates[card] + " but " + current_draft_id + " happened on " + current_draft_date + " (the draft has the wrong legality date)")
                     
         if card not in cardwins:
             cardwins[card] = wins
@@ -83,23 +114,39 @@ for line in f:
 
 outf = open("card stats output.tsv", "w")
 for (card, picks) in cardpicks.items():
-    avg = str((1.0 * sum(picks)) / len(picks))
-
+    
+    regularSum = 0
     sqInvSum = 0
     prioritySum = 0
+    possiblePicks = 0
+
+    release_date = card_release_dates[card]
+    for draft_date in draft_dates:
+        if release_date <= draft_date:
+            possiblePicks += 1
     
     for num in picks:
+        regularSum += num
         sqInvSum += (361 - num) * (361 - num)
         prioritySum += 1.0/num
+    
+    # each time it could have been picked and wasn't, it counts as being picked in spot 361
+    for i in range(possiblePicks - len(picks)):
+        regularSum += 361
+        sqInvSum += 0
+        prioritySum += 1.0/361
         
-    avgSqInv =  str(361 - math.sqrt((1.0 * sqInvSum) / len(picks)))
+    avg = str((1.0 * regularSum) / possiblePicks)    
+    avgSqInv =  str(361 - math.sqrt((1.0 * sqInvSum) / possiblePicks))
     
     carddata = carddict[card]
     cost = "-"
     if "mana_cost" in carddata:
         cost = carddata["mana_cost"]
+                
     outp = card + "\t" +\
            str(len(picks)) + "\t" +\
+           str(possiblePicks) + "\t" +\
            avg + "\t" +\
            avgSqInv + "\t" +\
            str(cardwins[card]) + "\t" +\
@@ -116,7 +163,7 @@ outf.close()
 
 seatoutf = open("seat stats output.tsv", 'w+')
 for seatidx in range(8):
-    seatoutf.write("seat " + str(seatidx) + "\t" + str(seatwins[seatidx]) + "\t" + str(seatlosses[seatidx]) + "\n")
+    seatoutf.write("seat " + str(seatidx+1) + "\t" + str(seatwins[seatidx]) + "\t" + str(seatlosses[seatidx]) + "\n")
 seatoutf.close()
 
     
