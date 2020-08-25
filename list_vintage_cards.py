b20f527d7a1df0f43a40d8f0f78abf28fe7207c6
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2020-08-25 06:39:52 -0700

    make the lister include banned cards if you set the right variable

--- list_vintage_cards.py
+++ list_vintage_cards.py
@@ -1,9 +1,13 @@
-﻿# usage: this makes a list of all cards currently legal in vintage, for use in a draft spreadsheet to highlight illegal picks
+﻿# usage: this makes a list of all cards currently legal in vintage, for use in a draft spreadsheet to highlight illegal picks.
+#        some cards were banned in vintage after being picked in past drafts. if you're analyzing old data rather than running
+#        a current draft, edit this file to set include_later_banned_cards = True, to include those cards
 import json,util.cardparse as cardparse
 
+include_later_banned_cards = False
 
 with open("input/scryfall-default-cards.json", encoding="utf-8") as json_file:
 
+    later_banned_file = open("cards-later-banned-in-vintage.txt", encoding="utf-8")
     outf = open("output/vintage_cards.txt", 'w', encoding="utf-8")
     jsoutf = open("output/vintage_cards.js", 'w', encoding="utf-8")
     jsoutf.write("vintage_cards = [\n")
@@ -22,7 +26,18 @@ with open("input/scryfall-default-cards.json", encoding="utf-8") as json_file:
                     jsoutf.write('"' + unquotename + '",\n')
                 except:
                     print(name + " had a problem")
-
+    if include_later_banned_cards:
+        for card in later_banned_file:
+            try:
+                name = card.rstrip()
+                outf.write(name+'\n')
+                unquotename = name.replace('"', '\\"')
+                jsoutf.write('"' + unquotename + '",\n')
+            except:
+                print(name + " had a problem (from cards-later-banned-in-vintage.txt)")
+        
+    
+    
     jsoutf.write("];")
     outf.close()
     jsoutf.close()
