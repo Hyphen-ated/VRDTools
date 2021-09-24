a7be668c031395dd8f626de19b0e8c3f94a88a11
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2021-09-24 03:42:52 -0700

    add update_from_gsheets_to_gsheets
    it uses the sheets api to read the raw data, invoke "compute_vrd_stats",
    and then write the results back to the sheet. automation baby.

--- compute_vrd_stats.py
+++ compute_vrd_stats.py
@@ -3,183 +3,192 @@
 #        card stats and seat stats, in the format expected for being pasted into my spreadsheet
 
 import math, json, datetime, util.cardparse as cardparse
-f = open("input/drafts.tsv", "r", encoding="utf-8")
-      
-playeridx = 0
-draftidx = 0
 
-cardwins = {}
-cardlosses = {}
-cardpicks = {}
-winpows = {}
-losspows = {}
-seatwins = [0] * 8
-seatlosses =[0] * 8
-card_release_dates = {}
-date_format = "%Y-%m-%d"
-picked_then_later_banned_cardnames = {}
+def compute_stats_of_file(path):
+    f = open(path, "r", encoding="utf-8")
+          
+    playeridx = 0
+    draftidx = 0
 
-with open("cards-later-banned-in-vintage.txt", encoding="utf-8") as ban_file:
-    for line in ban_file:
-        card = line.strip().lower()
-        picked_then_later_banned_cardnames[card] = True
+    cardwins = {}
+    cardlosses = {}
+    cardpicks = {}
+    winpows = {}
+    losspows = {}
+    seatwins = [0] * 8
+    seatlosses =[0] * 8
+    card_release_dates = {}
+    date_format = "%Y-%m-%d"
+    picked_then_later_banned_cardnames = {}
+
+    with open("cards-later-banned-in-vintage.txt", encoding="utf-8") as ban_file:
+        for line in ban_file:
+            card = line.strip().lower()
+            picked_then_later_banned_cardnames[card] = True
 
 
 
-carddict = {}
-with open("input/scryfall-default-cards.json", encoding="utf-8") as json_file:
-    data = json.load(json_file)
-    for card in data:
-        name = cardparse.cardname(card)
-        if cardparse.legal_in_vintage(card) or name in picked_then_later_banned_cardnames:
-            
-            carddict[name] = card
-            
-            #find the earliest date each card was released
-            carddatestr = card['released_at']
-            carddate = datetime.datetime.strptime(carddatestr, date_format)
-            if name not in card_release_dates.keys() or card_release_dates[name] > carddate:
-                card_release_dates[name] = carddate
-
-current_draft_id = None
-current_draft_date = None
-current_line = 0
-draft_dates = []
-for line in f:    
-    current_line += 1
-    elems = line.rstrip().split("\t")
-    draftinfo = elems[0] #  the draft id or date, on the first and second rows
-    if playeridx == 0:
-        if not draftinfo:
-            raise Exception("Missing draft id and date on line " + str(current_line))
-        infos = draftinfo.split(", ")
-        if len(infos) != 2:
-            raise Exception("Line " + str(current_line) + ": doesn't have draftid,date at the front")
-        current_draft_id = infos[0]
-        current_draft_date_str = infos[1]
-        if not current_draft_id or not current_draft_date_str:
-            raise Exception("Line " + str(current_line) + ": doesn't have draftid,date at the front")
-        current_draft_date = datetime.datetime.strptime(current_draft_date_str, date_format)
-        draft_dates.append(current_draft_date)        
-        
-    # elems[1] is player name        
-    winstr = elems[2]
-    lossstr = elems[3]
-    wins = int(winstr) if len(winstr) > 0 else 0
-    losses = int(lossstr) if len(lossstr) > 0 else 0
-    
-    seatwins[playeridx] += wins
-    seatlosses[playeridx] += losses
-
-    oddpick = True
-    cardcount = 0
-    for card in elems[4:]:
-        card = card.strip()
-        card = card.lower()
-        if not card or card == "unknown_pick":
-            continue
+    carddict = {}
+    with open("input/scryfall-default-cards.json", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        for card in data:
+            name = cardparse.cardname(card)
+            if cardparse.legal_in_vintage(card) or name in picked_then_later_banned_cardnames:
                 
-        if card_release_dates[card] > current_draft_date:
-            raise Exception(card + " was released on " + str(card_release_dates[card]) + " but " + current_draft_id + " happened on " + str(current_draft_date) + " (the draft has the wrong legality date)")
+                carddict[name] = card
+                
+                #find the earliest date each card was released
+                carddatestr = card['released_at']
+                carddate = datetime.datetime.strptime(carddatestr, date_format)
+                if name not in card_release_dates.keys() or card_release_dates[name] > carddate:
+                    card_release_dates[name] = carddate
+
+    current_draft_id = None
+    current_draft_date = None
+    current_line = 0
+    draft_dates = []
+    for line in f:            
+        current_line += 1
+        stripped_line = line.rstrip()
+        if not stripped_line: # ignore blank lines
+            continue        
+        elems = stripped_line.split("\t")
+        draftinfo = elems[0] #  the draft id or date, on the first and second rows
+        if playeridx == 0:
+            if not draftinfo:
+                raise Exception("Missing draft id and date on line " + str(current_line))
+            infos = draftinfo.split(", ")
+            if len(infos) != 2:
+                raise Exception("Line " + str(current_line) + ": doesn't have draftid,date at the front")
+            current_draft_id = infos[0]
+            current_draft_date_str = infos[1]
+            if not current_draft_id or not current_draft_date_str:
+                raise Exception("Line " + str(current_line) + ": doesn't have draftid,date at the front")
+            current_draft_date = datetime.datetime.strptime(current_draft_date_str, date_format)
+            draft_dates.append(current_draft_date)        
+            
+        # elems[1] is player name        
+        winstr = elems[2]
+        lossstr = elems[3]
+        wins = int(winstr) if len(winstr) > 0 else 0
+        losses = int(lossstr) if len(lossstr) > 0 else 0
+        
+        seatwins[playeridx] += wins
+        seatlosses[playeridx] += losses
+
+        oddpick = True
+        cardcount = 0
+        for card in elems[4:]:
+            card = card.strip()
+            card = card.lower()
+            if not card or card == "unknown_pick":
+                continue
                     
-        if card not in cardwins:
-            cardwins[card] = wins
-        else:
-            cardwins[card] += wins
+            if card_release_dates[card] > current_draft_date:
+                raise Exception(card + " was released on " + str(card_release_dates[card]) + " but " + current_draft_id + " happened on " + str(current_draft_date) + " (the draft has the wrong legality date)")
+                        
+            if card not in cardwins:
+                cardwins[card] = wins
+            else:
+                cardwins[card] += wins
 
-        if card not in cardlosses:
-            cardlosses[card] = losses
-        else:
-            cardlosses[card] += losses
+            if card not in cardlosses:
+                cardlosses[card] = losses
+            else:
+                cardlosses[card] += losses
 
-        if oddpick:
-            pick = playeridx + 1 + cardcount * 8
-        else:
-            pick = (8 - playeridx) + cardcount * 8
+            if oddpick:
+                pick = playeridx + 1 + cardcount * 8
+            else:
+                pick = (8 - playeridx) + cardcount * 8
 
-        if card not in cardpicks:
-            cardpicks[card] = [pick]
-        else:
-            cardpicks[card].append(pick)
+            if card not in cardpicks:
+                cardpicks[card] = [pick]
+            else:
+                cardpicks[card].append(pick)
 
-        winpow = 1.0/(pick + 10) * wins
-        losspow = 1.0/(pick + 10) * losses
-        if card not in winpows:
-            winpows[card] = winpow
-        else:
-            winpows[card] += winpow
+            winpow = 1.0/(pick + 10) * wins
+            losspow = 1.0/(pick + 10) * losses
+            if card not in winpows:
+                winpows[card] = winpow
+            else:
+                winpows[card] += winpow
 
-        if card not in losspows:
-            losspows[card] = losspow
-        else:
-            losspows[card] += losspow
+            if card not in losspows:
+                losspows[card] = losspow
+            else:
+                losspows[card] += losspow
 
-        oddpick = not oddpick
-        cardcount += 1
-           
-    playeridx += 1
-    if playeridx >= 8:
-        playeridx = 0
+            oddpick = not oddpick
+            cardcount += 1
+               
+        playeridx += 1
+        if playeridx >= 8:
+            playeridx = 0
 
-outf = open("output/card stats.tsv", "w", encoding="utf-8")
-print("Writing output/card stats.tsv")
-for (card, picks) in cardpicks.items():
-    
-    regularSum = 0
-    sqInvSum = 0
-    prioritySum = 0
-    possiblePicks = 0
-
-    release_date = card_release_dates[card]
-    for draft_date in draft_dates:
-        if release_date <= draft_date:
-            possiblePicks += 1
-    
-    for num in picks:
-        regularSum += num
-        sqInvSum += (361 - num) * (361 - num)
-        prioritySum += 1.0/num
-    
-    # each time it could have been picked and wasn't, it counts as being picked in spot 361
-    for i in range(possiblePicks - len(picks)):
-        regularSum += 361
-        sqInvSum += 0
-        prioritySum += 1.0/361
+    outf = open("output/card stats.tsv", "w", encoding="utf-8")
+    print("Writing output/card stats.tsv")
+    for (card, picks) in cardpicks.items():
         
-    avg = str((1.0 * regularSum) / possiblePicks)    
-    avgSqInv =  str(361 - math.sqrt((1.0 * sqInvSum) / possiblePicks))
-    
-    carddata = carddict[card]
-    cost = "-"
-    if "mana_cost" in carddata:
-        cost = carddata["mana_cost"]
-    cmc = carddata["cmc"]       
-    coloridentity = "".join(carddata["color_identity"])
-                
-    outp = card + "\t" +\
-           str(len(picks)) + "\t" +\
-           str(possiblePicks) + "\t" +\
-           avg + "\t" +\
-           avgSqInv + "\t" +\
-           str(cardwins[card]) + "\t" +\
-           str(cardlosses[card]) + "\t" +\
-           str(winpows[card]) + "\t" +\
-           str(losspows[card]) + "\t" +\
-           cost + "\t" +\
-           coloridentity + "\t" +\
-           str(cmc) + "\n"
-           
+        regularSum = 0
+        sqInvSum = 0
+        prioritySum = 0
+        possiblePicks = 0
 
-    
-    outf.write(outp)
+        release_date = card_release_dates[card]
+        for draft_date in draft_dates:
+            if release_date <= draft_date:
+                possiblePicks += 1
+        
+        for num in picks:
+            regularSum += num
+            sqInvSum += (361 - num) * (361 - num)
+            prioritySum += 1.0/num
+        
+        # each time it could have been picked and wasn't, it counts as being picked in spot 361
+        for i in range(possiblePicks - len(picks)):
+            regularSum += 361
+            sqInvSum += 0
+            prioritySum += 1.0/361
+            
+        avg = str((1.0 * regularSum) / possiblePicks)    
+        avgSqInv =  str(361 - math.sqrt((1.0 * sqInvSum) / possiblePicks))
+        
+        carddata = carddict[card]
+        cost = "-"
+        if "mana_cost" in carddata:
+            cost = carddata["mana_cost"]
+        cmc = carddata["cmc"]       
+        coloridentity = "".join(carddata["color_identity"])
+                    
+        outp = card + "\t" +\
+               str(len(picks)) + "\t" +\
+               str(possiblePicks) + "\t" +\
+               avg + "\t" +\
+               avgSqInv + "\t" +\
+               str(cardwins[card]) + "\t" +\
+               str(cardlosses[card]) + "\t" +\
+               str(winpows[card]) + "\t" +\
+               str(losspows[card]) + "\t" +\
+               cost + "\t" +\
+               coloridentity + "\t" +\
+               str(cmc) + "\n"
+               
 
-outf.close()
+        
+        outf.write(outp)
 
-seatoutf = open("output/seat stats.tsv", 'w', encoding="utf-8")
-print("Writing output/seat stats.tsv")
-for seatidx in range(8):
-    seatoutf.write("seat " + str(seatidx+1) + "\t" + str(seatwins[seatidx]) + "\t" + str(seatlosses[seatidx]) + "\n")
-seatoutf.close()
+    outf.close()
+
+    seatoutf = open("output/seat stats.tsv", 'w', encoding="utf-8")
+    print("Writing output/seat stats.tsv")
+    for seatidx in range(8):
+        seatoutf.write("seat " + str(seatidx+1) + "\t" + str(seatwins[seatidx]) + "\t" + str(seatlosses[seatidx]) + "\n")
+    seatoutf.close()
+
+
+if __name__ == "__main__":
+    compute_stats_of_file("input/drafts.tsv")
 
     
         
---
+++ update_from_gsheets_to_gsheets.py
@@ -0,0 +1,163 @@
+# Usage: after manually editing "Raw Data For Stats" tab to cover new drafts,
+#        run this tool to fully automatically update "Card Stats" and "Seat Stats"
+
+import pickle
+import os.path
+import csv
+import compute_vrd_stats
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID of the stats spreadsheet (this one is a test copy)
+spreadsheet_id = '10ggtuvvL4q6A5Ub3Ddx7XcEtlpRJMD79DWoCuuq5pv0'
+all_drafts_range = 'Raw Material For Stats!A103:CA'
+
+cur_service = None
+def get_service():
+    global cur_service
+    if cur_service is None:
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('input/token.pickle'):
+            with open('input/token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'input/gsheet_credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('input/token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        cur_service = build('sheets', 'v4', credentials=creds)
+    return cur_service
+
+def scrape_drafts_tsv():
+    service = get_service()
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                range=all_drafts_range).execute()
+    values = result.get('values', [])
+
+    if not values:
+        raise Exception('No draft data found in the google sheet')
+         
+    outf = open("output/scraped_drafts.tsv", "w", encoding="utf-8")        
+    tsv_output = csv.writer(outf, delimiter='\t')
+    tsv_output.writerows(values)
+    print ("Wrote basic draft data to output/scraped_drafts.tsv")
+    return len(values)
+
+# the api demands we use a numeric id for the tabs. this helper function lets me retrieve the id given a known name
+def get_sheet_id_by_name(service, name):
+    spreadsheet = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    for _sheet in spreadsheet['sheets']:
+        if _sheet['properties']['title'] == name:
+            return _sheet['properties']['sheetId']    
+
+# if it's a number, insert it into sheets as a number. otherwise as a string.
+def describe_val(v):
+    if v.replace('.','',1).isdigit():
+        return {'numberValue': v}
+    else:
+        return {'stringValue': v}
+
+# same method for both card stats and seat stats.
+def upload_sheet_data(sheetName, startRowIdx, startColIdx, nestedArrays):
+    service = get_service()
+    maxHeight = len(nestedArrays) + 1 # extra for header row
+    maxWidth = len(nestedArrays[0])
+
+    sheetId = get_sheet_id_by_name(service, sheetName)
+
+    update_rows = [{'values': [{'userEnteredValue': describe_val(f)} for f in e]} for e in nestedArrays]
+    update_range = {'sheetId': sheetId, 'startRowIndex': startRowIdx, 'startColumnIndex': startColIdx, 'endColumnIndex': startColIdx + maxWidth}
+    body = {
+        'requests': [
+            {
+                'updateSheetProperties': { # first make sure the sheet is big enough
+                    'properties': {
+                        'sheetId': sheetId,
+                        'gridProperties': {
+                            'rowCount': maxHeight
+                        }
+                    },
+                    "fields": "gridProperties(rowCount)"
+                }
+            },
+            {
+                "copyPaste": { # if we expanded it, we need to copy the formulas on the side to the new area
+                    "source": {
+                        "sheetId": sheetId,
+                        "startRowIndex": 1, # grab the first line
+                        "endRowIndex": 2
+                    },
+                    "destination": {
+                        "sheetId": sheetId,
+                        "startRowIndex": 2,
+                        "endRowIndex": maxHeight + 1 # paste the formulas down to the bottom
+                    },
+                    "pasteType": "PASTE_FORMULA"
+                }
+            },
+            {    # add the actual data
+                'updateCells': {'rows': update_rows, 'range': update_range, 'fields': 'userEnteredValue'}
+            },
+            {
+                "sortRange": {
+                    "range": {
+                        "sheetId": sheetId,
+                        "startRowIndex": 1,
+                    },
+                    "sortSpecs": [
+                        {
+                            "dimensionIndex": 4,    # column 4 is coincidentally a good sort key for cards AND seats
+                            "sortOrder": "ASCENDING"
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    
+    request = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body=body)
+    request.execute()
+
+
+def read_tsv_to_arrs(filename):
+    outarr = []
+    with open(filename, 'r') as f:
+        for line in f:
+            arr = line.split("\t")            
+            outarr.append([s.strip() for s in arr])
+    return outarr
+    
+if __name__ == '__main__':   
+    print("Getting basic draft data from google sheet...")
+    rows = scrape_drafts_tsv()
+    print("Found " + str(rows) + " rows (" + str(rows / 8) + " drafts)")
+    compute_vrd_stats.compute_stats_of_file('output/scraped_drafts.tsv')
+    doit = input("Inspect those stats files now. Upload them to the sheet? (y/N)")
+    if doit.lower() == "y":
+        print ("Uploading card data...")
+        card_data = read_tsv_to_arrs("output/card stats.tsv")
+        upload_sheet_data("Card Stats", 1, 1, card_data)
+
+        print ("Uploading seat data...")
+        seat_data = read_tsv_to_arrs("output/seat stats.tsv")
+        upload_sheet_data("Seat Stats", 1, 0, seat_data)
+
+        print("Done")
+    else:
+        print ("Okay, not uploading. Done.")
+    
